6747a84f25dc1134b81c2d90ea20fa96d32acc07
andyanogifaro <55392024+andyanogifaro@users.noreply.github.com>
2022-06-30 19:31:43 +0700

    Update App.java

--- simple-java-maven-app-master/src/main/java/com/mycompany/app/App.java
+++ simple-java-maven-app-master/src/main/java/com/mycompany/app/App.java
@@ -16,6 +16,11 @@ public class App
 
     private final String getMessage() {
         return message;
+        return message;
+        return message;
+        return message;
+        return message;
+        return message;
     }
 
 }
